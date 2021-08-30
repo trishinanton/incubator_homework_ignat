be6dcdde1d80ae12eb7407827709fc375b6ef083
trishinanton <tosha.trishin@bk.ru>
2021-08-30 22:08:01 +0300

    Updates

---
+++ precache-manifest.5cebd721cd40cfb85408f6edbbf3eb45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be342685c581aa68e3750cc35974d4f",
+    "url": "/incubator_homework_ignat/index.html"
+  },
+  {
+    "revision": "dfa49959a655ac9298ab",
+    "url": "/incubator_homework_ignat/static/css/main.ca00f145.chunk.css"
+  },
+  {
+    "revision": "eddcfe2b3af3f3787811",
+    "url": "/incubator_homework_ignat/static/js/2.6562ebed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/incubator_homework_ignat/static/js/2.6562ebed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa49959a655ac9298ab",
+    "url": "/incubator_homework_ignat/static/js/main.9f207b8d.chunk.js"
+  },
+  {
+    "revision": "dacdad2749add559152a",
+    "url": "/incubator_homework_ignat/static/js/runtime-main.423c1f5a.js"
+  }
+]);
